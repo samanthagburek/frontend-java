5d98526d7cc2d24cfcffacd7d54d04c2df3a2bf5
samanthagburek <sgg2056@nyu.edu>
2025-04-29 01:38:45 -0400

    First commit

---
+++ src/main/java/com/example/frontendjava/FrontendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/frontendjava/FrontendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
